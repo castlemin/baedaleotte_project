6bdfd95e10dc18bf4acbf7b4f216e6ab0ba7002e
d_flex <hiyoo95@gmail.com>
2022-01-03 18:48:47 +0900

    프론트엔드 네비게이션 추가, 카테고리페이지,리포트 페이지 UI 수정
    Former-commit-id: 655d67b36b9f60d2c84dfe1240b3e1a9777b9d6d

--- client/src/pages/service/regional-page/regional-category/RegionalCategoryPage.component.tsx
+++ client/src/pages/service/regional-page/regional-category/RegionalCategoryPage.component.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import { CategoryListContainer } from './RegionalCategory.styles';
+
+import { weekDay, hour } from '../../../../assets/data/weekDay';
+import {
+  TitleContainer,
+  DescContainer,
+} from '../../../../components/UI/Text/Text.styles';
+import Header from '../../../../components/UI/header/Header.component';
+import { categories } from '../../../../assets/data/categories';
+import { Card } from '../../../../components/UI/Card/Card.styles';
+import { NextButton } from './RegionalCategory.styles';
+import { useNavigate } from 'react-router-dom';
+
 const RegionalCategoryPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleToShopList = () => {
+    navigate('/service/regional/shop_list');
+  };
+
   return (
-    <CategoryListContainer>
-      <div></div>
-    </CategoryListContainer>
+    <>
+      <Header serviceStatic />
+      <TitleContainer>
+        오늘은 {weekDay}, 지금 {hour}시 인기 메뉴는
+      </TitleContainer>
+      <CategoryListContainer>
+        {categories.map((cat) => (
+          <Card category>
+            <DescContainer>{cat}</DescContainer>
+          </Card>
+        ))}
+      </CategoryListContainer>
+      <NextButton onClick={handleToShopList}>임시 버튼</NextButton>
+    </>
   );
 };
 
