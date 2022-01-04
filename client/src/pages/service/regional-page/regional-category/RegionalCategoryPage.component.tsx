93a0f222cf3c9c5288b36b15258fbba0720ad67a
d_flex <hiyoo95@gmail.com>
2022-01-05 02:55:38 +0900

    separated imageMapper and moved to data dir
    Former-commit-id: 2dbd042d0b476e3696337ca090f377b7932c6406

--- client/src/pages/service/regional-page/regional-category/RegionalCategoryPage.component.tsx
+++ client/src/pages/service/regional-page/regional-category/RegionalCategoryPage.component.tsx
@@ -13,41 +13,7 @@ import Modal from '../../../../components/UI/modal/Modal.component';
 import BackDrop from '../../../../components/UI/BackDrop/BackDrop.component';
 import { NextButton } from './RegionalCategory.styles';
 import { useNavigate } from 'react-router-dom';
-import pork_url from '../../../../assets/images/pork_feet.jpeg';
-import night_food_url from '../../../../assets/images/night_food.jpeg';
-import stew_url from '../../../../assets/images/stew.png';
-import japanese_url from '../../../../assets/images/japanese.jpeg';
-import chicken_url from '../../../../assets/images/fried_chicken.jpg';
-import sushi_url from '../../../../assets/images/sushi.jpeg';
-import snack_url from '../../../../assets/images/snack.jpeg';
-import pizza_url from '../../../../assets/images/pizza.jpeg';
-import dessert_url from '../../../../assets/images/dessert.jpeg';
-import porridge_url from '../../../../assets/images/porridge.jpeg';
-import asian_url from '../../../../assets/images/asian.png';
-import burger_url from '../../../../assets/images/burger.jpeg';
-import chinese_url from '../../../../assets/images/chinese.jpeg';
-import korean_url from '../../../../assets/images/korean.jpeg';
-
-interface imageUrl {
-  [index: string]: string;
-}
-
-const IMAGES: imageUrl = {
-  족발보쌈: pork_url,
-  야식: night_food_url,
-  찜탕: stew_url,
-  일식돈까스: japanese_url,
-  치킨: chicken_url,
-  회초밥: sushi_url,
-  한식: korean_url,
-  분식: snack_url,
-  피자양식: pizza_url,
-  중식: chinese_url,
-  카페디저트: dessert_url,
-  도시락죽: porridge_url,
-  아시안: asian_url,
-  버거: burger_url,
-};
+import { IMAGES } from '../../../../assets/data/imgMapper';
 
 const RegionalCategoryPage = () => {
   const navigate = useNavigate();
