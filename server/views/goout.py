caec0b7376d7c0509190965a0f4c74958d8062e5
최성민 <csm1165@naver.com>
2022-01-06 20:33:24 +0900

    feat : modified views 1. modified SQLalchemy query filtering method 2. modified getting post data method (from -> json)
    Former-commit-id: f812243c73df9a475d8ec072e27cb4a853183236

--- server/views/goout.py
+++ server/views/goout.py
@@ -1,8 +1,9 @@
 import json
-import re
 
 from flask import Blueprint, request
 from flask.wrappers import Response
+from sqlalchemy import or_
+
 from models.goout import GoOut, GoOutReviews
 from utility.todict import AlchemyEncoder
 from api_requests import geocoding
@@ -10,27 +11,27 @@ from api_requests import geocoding
 bp = Blueprint("goout", __name__, url_prefix="/goout")
 
 
-def get_goouts_by_categories(cat1: str, cat2: str):
+def get_goouts_by_filtering(region: str, cat1: str, cat2: str):
     if cat1 is None:
-        return GoOut.query.all()
+        return GoOut.query.filter(GoOut.region == region).all()
     elif cat2 is None:
-        return GoOut.query.filter(GoOut.categories.ilike(f"%{cat1}"))
+        return GoOut.query.filter(GoOut.region == region)\
+            .filter(GoOut.category == cat1).all()
     else:
-        return GoOut.query.filter(
-            GoOut.categories.ilike(f"%{cat1}") | GoOut.categories.ilike(f"%{cat2}")
-        ).distinct()
+        return GoOut.query.filter(GoOut.region == region)\
+            .filter(or_(GoOut.category == cat1, GoOut.category == cat2)).all()
 
 
 @bp.route('', methods=['POST'])
 def get_goout():
-    lat = float(request.form['lat'])
-    lng = float(request.form['lng'])
+    lat = request.json['lat']
+    lng = request.json['lng']
     region = geocoding.getKoreanJCG(lat, lng)
 
     cat_1 = request.args.get('category1')
     cat_2 = request.args.get('category2')
 
-    res = get_goouts_by_categories(cat_1, cat_2)
+    res = get_goouts_by_filtering(region, cat_1, cat_2)
     res = json.dumps(res, cls=AlchemyEncoder, ensure_ascii=False, indent=4)
 
     return Response(res, mimetype="application/json")
@@ -44,9 +45,10 @@ def get_goout_detail(goout_id: int):
     return Response(res, mimetype="application/json")
 
 
-@bp.route("/review/<int:goout_id", methods=['GET'])
+@bp.route("/reviews/<int:goout_id>", methods=['GET'])
 def get_goout_review(goout_id: int):
-    res = GoOutReviews.query.filter(GoOutReviews.goout_id == goout_id).all()
+    goout = GoOut.query.filter(GoOut.id == goout_id).first()
+    res = GoOutReviews.query.filter(GoOutReviews.store_name == goout.name).all()
     res = json.dumps(res, cls=AlchemyEncoder, ensure_ascii=False, indent=4)
 
     return Response(res, mimetype="application/json")
