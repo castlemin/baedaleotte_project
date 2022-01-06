173b991272863de1163aeea6d38ec6faed8c4d78
최성민 <csm1165@naver.com>
2022-01-07 02:22:45 +0900

    feat: added scheduling and modified views 1. scheduled importing 2. modified URLs
    Former-commit-id: 561f886ca0bd85283d2e752658ecf67b08d70d5a

--- server/views/graph.py
+++ server/views/graph.py
@@ -1,55 +1,81 @@
 # 데이터분석에서 작성한 py 파일 import
 import imp
 import importlib.util
+from flask import Blueprint, session, request, Response, jsonify
+from apscheduler.schedulers.background import BackgroundScheduler
+from api_requests.geocoding import getKoreanJCG
 
 spec = importlib.util.spec_from_file_location("da", "dataanalysis/data_analysis.py")
 da = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(da)
 
-from flask import Blueprint
 
-# imp.reload(da)
-# 나중에 윤상님이 변환함수 알려주시면 바꿔야 함
-region = "관악구"
+def import_schedule():
+    try:
+        spec.loader.exec_module(da)
+        print("importing success")
+    except:
+        print("importing failed")
+
+
+sched = BackgroundScheduler(timezone="Asia/Seoul")
+sched.start()
+sched.add_job(import_schedule, trigger='cron', hour=0, minute=10)
+
 
 bp = Blueprint("graph", __name__, url_prefix="/data")
 
 
-@bp.route("/get_vac")
+@bp.route("/user_location", methods=['POST'])
+def get_user_location():
+    lat = request.json['lat']
+    lng = request.json['lng']
+
+    region = getKoreanJCG(lat, lng)
+
+    res = {
+        "region": region
+    }
+
+    return jsonify(res)
+
+
+@bp.route("/vac")
 def get_vac():
     return da.백신현황(da.vac_data)
 
 
-@bp.route("/get_seoul_risk_map_all")
+@bp.route("/seoul_risk_map_all")
 def get_seoul_risk_map_all():
     return da.서울코로나위험도지도(da.seoul_corona_score)
 
 
 # gps 부분에 gps정보가 들어와야 함
-@bp.route("/get_seoul_risk_map/gps")
+@bp.route("/seoul_risk_map")
 def get_seoul_risk_map():
-    # return da.서울코로나위험도지도(da.seoul_corona_score, GPS함수(gps정보))
+    region = request.args['region']
     return da.서울코로나위험도지도(da.seoul_corona_score, region)
 
 
-@bp.route("/get_risk_rank")
+@bp.route("/risk_rank")
 def get_risk_rank():
+    region = request.args['region']
     return da.위험도순위(da.seoul_corona_score, region)
 
 
-@bp.route("/get_coronic_all")
+@bp.route("/coronic_all")
 def get_coronic_all():
     return da.내지역확진자all(da.corona_data, "서울")
 
 
-@bp.route("/get_coronic_gu")
+@bp.route("/coronic_gu")
 def get_coronic_gu():
+    region = request.args['region']
     return da.내지역확진자all(da.corona_data, region)
 
 
-@bp.route("/get_risk_score")
+@bp.route("/risk_score")
 def get_risk_score():
+    region = request.args['region']
     return da.위험도점수(da.seoul_corona_score, region)
 
-
-# print(da.위험도점수(da.seoul_corona_score, region))
