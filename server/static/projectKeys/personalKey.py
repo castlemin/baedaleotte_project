7d65ad61490ed0b17cb04db22853df530cd80c3a
sy109 <s-yoon3@wiu.edu>
2022-01-18 04:20:37 +0900

    배포완료
    Former-commit-id: 5422587d7840a102485f7795250554c91e640f69

---
+++ server/static/projectKeys/personalKey.py
@@ -0,0 +1,19 @@
+"""
+personal keys
+"""
+
+
+db_setting = {
+    "user": "team04",
+    "password": "1234",
+    "host": "localhost",
+    "port": 3306,
+    "database": "TEAM04",
+}
+SEOUL_OPEN_DATA_PERSONAL_KEY = "7847747475776f753435454f6b6179"
+
+secretKey = "1234"
+
+geocodingKey = "AIzaSyDmKk-Vine19NYMkRabXbYCrCVik36V-o4"
+
+sentry_dsn = "https://ae48aa0a59f54fcc93acb1c8383b2585@o1106875.ingest.sentry.io/6133634"
